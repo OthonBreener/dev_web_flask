bbc3f6f7036cab461427efc14598dbe434cb14df
Othon <othonbrener123@gmail.com>
2022-01-17 15:03:07 -0300

    FIX: Adicionando orm com iversao de dependencias

---
+++ aplicacao/pedido/orm.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import MetaData, Table, Column, Integer, String
+
+# ! O Orm conhece o domínio e não o contrário ( Inversão de dependências )
+from models import Pedido
+
+metadata = MetaData()
+
+pedidos = Table(
+    'pedidos',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('id_pedido', String(255)),
+    Column('identificador', String(255)),
+    Column('quantidade', Integer, nullable=False)
+)
+
+
+def start_mappers():
+    mapenado_pedidos = mapper(Pedido, pedidos)
---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+from pytest import fixture
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+
+from aplicacao.pedido.orm import start_mappers, metadata
+
+@fixture
+def in_memory_db():
+    engine = create_engine('sqlite:///:memory:')
+    metadata.create_all(engine)
+    return engine
+
+@fixture
+def session(in_memory_db):
+
+    start_mappers()
+    yield sessionmaker(bind=in_memory_db)()
+    clear_mappers()
