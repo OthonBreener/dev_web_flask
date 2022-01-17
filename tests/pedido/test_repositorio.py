93d654713208e6fd362ca4e503bf741f65cc486c
Othon <othonbrener123@gmail.com>
2022-01-17 17:47:04 -0300

    FIX: Adicionando arquivos nao salvos

--- tests/pedido/test_repositorio.py
+++ tests/pedido/test_repositorio.py
@@ -1,7 +1,7 @@
 from pytest import mark
 from datetime import datetime
 from aplicacao.alocacoes.dominio.models import LoteMercadoriaDisponivel
-from aplicacao.alocacoes.repositorio import RepositorioSqlAlchemy, RepositorioFake
+from aplicacao.alocacoes.adaptadores.repositorio import RepositorioSqlAlchemy, RepositorioFake
 
 @mark.task
 def test_repositorio_pode_salvar_um_lote_de_mercadorias(session):
