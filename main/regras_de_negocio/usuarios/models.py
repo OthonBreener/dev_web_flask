bd71fdd9c14c8fa819c11172f77e0ae8583dca86
othonbreener <othonbrener123@gmail.com>
2022-01-25 10:12:36 -0300

    Adicionando modelo de confirmaçao de usuarios

--- main/regras_de_negocio/usuarios/models.py
+++ main/regras_de_negocio/usuarios/models.py
@@ -1,3 +1,5 @@
+from flask import current_app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from main import database
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -11,6 +13,7 @@ class Usuario(UserMixin, database.Model):
     email = database.Column(database.String(64), unique=True, index=True)
     user_name = database.Column(database.String(64), unique=True, index=True)
     password_hash = database.Column(database.String(128))
+    confirmed = database.Column(database.Boolean, default=False)
 
     def __repr__(self) -> str:
         return '<Usuario %r>' % self.user_name
@@ -28,6 +31,27 @@ class Usuario(UserMixin, database.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+    def gerador_de_token_de_confirmacao(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm':self.id}).decode('utf-8')
+    
+
+    def confirmar_conta(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            dados = s.loads(token.encode('utf-8'))
+        except:
+            return False
+
+        if dados.get('confirm') != self.id:
+            return False
+        
+        self.confirm = True
+        database.session.add(self)
+        return True
+
 
 @login_manager.user_loader
 def load_user(user_id):
